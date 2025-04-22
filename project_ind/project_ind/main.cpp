b3b52e71bc0399aa98c94f5e67d2404a447af980
unknown <dumpling.lol13@gmail.com>
2025-04-22 22:13:31 +0300

    add new OP project? add new decisions

---
+++ project_ind/project_ind/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "time_for_registration.h"
+#include "time_for_registration.cpp"
+#include "FIO.h"
+#include "FIO.cpp"
+#include "calendar_of_dates.h"
+#include "calendar_of_dates.cpp"
+#include "client's_documents.h"
+#include "client's_documents.cpp"
+#include "doctors.h"
+#include "doctors.cpp"
+#include "client.h"
+#include "client.cpp"
+#include "employee.h"
+#include "employee.cpp"
+#include "visiting.cpp"
+#include "visiting.h"
+#include "date.h"
+#include "date.cpp"
+#include "cabinet.cpp"
+#include "cabinet.h"
+#include "equipment.cpp"
+#include "equipment.h"
+#include "schedule.cpp"
+#include "schedule.h"
+
+
+#include <cstdio>
+#include <Windows.h>
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	//Time time;
+	//FIO person;
+	//Date date;
+}
