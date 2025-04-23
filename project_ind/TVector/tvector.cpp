95c5f609955350ea6885bac2f9f357915beef53b
unknown <dumpling.lol13@gmail.com>
2025-04-23 09:09:17 +0300

    added implementation of setters and getters

--- project_ind/TVector/tvector.cpp
+++ project_ind/TVector/tvector.cpp
@@ -30,3 +30,98 @@ template<class T>
 TVector<T>::~TVector() {
     delete[] data;
 }
+template<class T>
+void TVector<T>::resize(size_t new_capacity) {
+    T* new_data = new T[new_capacity];
+    size_t new_size = std::min(size, new_capacity);
+    if (data != nullptr) {
+        std::copy(data, data + new_size, new_data);
+    }
+    delete[] data;
+    data = new_data;
+    capacity = new_capacity;
+    size = new_size;
+}
+
+template<class T>
+void TVector<T>::push_back(const T& value) {
+    if (size == capacity) {
+        resize(capacity == 0 ? 1 : capacity * 2);
+    }
+    data[size++] = value;
+}
+
+template<class T>
+void TVector<T>::pop_back() {
+    if (size > 0) {
+        --size;
+    }
+}
+
+template<class T>
+void TVector<T>::clear() {
+    size = 0;
+}
+
+template<class T>
+bool TVector<T>::empty() const {
+    return size == 0;
+}
+
+template<class T>
+size_t TVector<T>::get_size() const {
+    return size;
+}
+
+template<class T>
+size_t TVector<T>::get_capacity() const {
+    return capacity;
+}
+
+template<class T>
+T& TVector<T>::at(size_t index) {
+    if (index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data[index];
+}
+
+template<class T>
+const T& TVector<T>::at(size_t index) const {
+    if (index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data[index];
+}
+
+template<class T>
+void TVector<T>::reserve(size_t new_capacity) {
+    if (new_capacity > capacity) {
+        resize(new_capacity);
+    }
+}
+template<class T>
+T& TVector<T>::operator[](size_t index) {
+    return data[index];
+}
+
+template<class T>
+const T& TVector<T>::operator[](size_t index) const {
+    return data[index];
+}
+template<class T>
+TVector<T>& TVector<T>::operator=(const TVector& other) {
+    if (this != &other) {
+        delete[] data;
+        size = other.size;
+        capacity = other.capacity;
+        if (other.capacity > 0) {
+            data = new T[other.capacity];
+            std::copy(other.data, other.data + other.size, data);
+        }
+        else {
+            data = nullptr;
+        }
+    }
+    return *this;
+}
--- project_ind/project_ind/main.cpp
+++ project_ind/project_ind/main.cpp
@@ -30,11 +30,130 @@
 #include <cstdio>
 #include <Windows.h>
 
+void set_color(int text_color, int bg_color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, (bg_color << 4) | text_color);
+}
+
+namespace TestSystem {
+    int count_success = 0, count_failed = 0;
+
+    void start_test(bool(*test)(), const char* name_of_test) {
+        set_color(2, 0);
+        std::cout << "[ RUN      ]";
+        set_color(7, 0);
+        std::cout << name_of_test << std::endl;
+
+        bool status = test();
+
+        if (status == true) {
+            set_color(2, 0);
+            std::cout << "[       OK ]" << std::endl;
+            count_success++;
+        }
+        else {
+            set_color(4, 0);
+            std::cout << "[  FAILED  ]" << std::endl;
+            count_failed++;
+        }
+        set_color(7, 0);
+    }
+
+    template <class T>
+    bool check(const T& expected, const T& actual) {
+        if (expected == actual) {
+            return true;
+        }
+        else {
+            std::cerr << "Expected result is " << expected << ", but actual is " << actual << "." << std::endl;
+            return false;
+        }
+    }
+
+    void print_init_info() {
+        set_color(2, 0);
+        std::cout << "[==========] " << std::endl;
+        set_color(7, 0);
+    }
+
+    void print_final_info() {
+        set_color(2, 0);
+        std::cout << "[==========] ";
+        set_color(7, 0);
+        std::cout << count_success + count_failed << " test" << (count_success + count_failed > 1 ? "s" : "") << " ran." << std::endl;
+        set_color(2, 0);
+        std::cout << "[  PASSED  ] ";
+        set_color(7, 0);
+        std::cout << count_success << " test" << (count_success > 1 ? "s" : "") << std::endl;
+        if (count_failed > 0) {
+            set_color(4, 0);
+            std::cout << "[  FAILED  ] ";
+            set_color(7, 0);
+            std::cout << count_failed << " test" << (count_failed > 1 ? "s." : ".") << std::endl;
+        }
+    }
+};
+
+// Тест конструктора по умолчанию
+bool test_1_default_constructor() {
+    TVector<int> vec;
+    bool expected_result = true;
+    bool actual_result = (vec.get_size() == 0 && vec.get_capacity() == 0);
+    return TestSystem::check(expected_result, actual_result);
+}
+
+// Тест конструктора с начальным размером
+bool test_2_size_constructor() {
+    TVector<int> vec(5);
+    bool expected_result = true;
+    bool actual_result = (vec.get_size() == 5 && vec.get_capacity() == 5);
+    return TestSystem::check(expected_result, actual_result);
+}
+
+// Тест конструктора копирования
+bool test_3_copy_constructor() {
+    TVector<int> original;
+    original.push_back(1);
+    original.push_back(2);
+    original.push_back(3);
+
+    TVector<int> copy(original);
+    bool expected_result = true;
+    bool actual_result = (copy.get_size() == original.get_size() &&
+        copy.get_capacity() == original.get_capacity() &&
+        copy[0] == original[0] &&
+        copy[1] == original[1] &&
+        copy[2] == original[2]);
+    return TestSystem::check(expected_result, actual_result);
+}
+
+// Тест оператора присваивания
+bool test_4_assignment_operator() {
+    TVector<int> original;
+    original.push_back(1);
+    original.push_back(2);
+    original.push_back(3);
+
+    TVector<int> copy;
+    copy = original;
+    bool expected_result = true;
+    bool actual_result = (copy.get_size() == original.get_size() &&
+        copy.get_capacity() == original.get_capacity() &&
+        copy[0] == original[0] &&
+        copy[1] == original[1] &&
+        copy[2] == original[2]);
+    return TestSystem::check(expected_result, actual_result);
+}
 
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	//Time time;
-	//FIO person;
-	//Date date;
+    TestSystem::print_init_info();
+
+    TestSystem::start_test(test_1_default_constructor, "TVector.test_1_default_constructor");
+    TestSystem::start_test(test_2_size_constructor, "TVector.test_2_size_constructor");
+    TestSystem::start_test(test_3_copy_constructor, "TVector.test_3_copy_constructor");
+    TestSystem::start_test(test_4_assignment_operator, "TVector.test_4_assignment_operator");
+
+    TestSystem::print_final_info();
+    system("pause");
+    return 0;
 }
